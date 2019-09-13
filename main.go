058c3db39a5c05fc80fdc739dd6c556878cbb5bc
Joost van der Griendt <joostvdg@gmail.com>
2019-09-13 10:10:51 +0200

    change message
    Signed-off-by: Joost van der Griendt <joostvdg@gmail.com>

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Welcome Kubernetes Days!!"
+	title := "Welcome to Kubernetes Days!"
 
 	from := ""
 	if r.URL != nil {
